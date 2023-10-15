53bec9b6277caaf50103bd9dd7f63be3bf23ce1c
nwjbrandon <nwjbrandon@gmail.com>
2023-10-15 14:47:13 +0800

    update

--- models.py
+++ models.py
@@ -5,13 +5,21 @@ class FeatureExtractor(nn.Module):
     def __init__(self):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Conv2d(1, 32, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(32),
-            nn.MaxPool2d(2),
-            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.Conv2d(1, 64, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.BatchNorm2d(64),
+            nn.MaxPool2d(2),
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(128),
+            nn.MaxPool2d(2),
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(256),
+            nn.MaxPool2d(2),
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(512),
             nn.MaxPool2d(2),
         )
 
@@ -29,7 +37,7 @@ class ClassificationModel(nn.Module):
         self.prediction = nn.Sequential(
             nn.AdaptiveAvgPool2d(1),
             nn.Flatten(start_dim=1),
-            nn.Linear(64, self.hyperparams["n_out"]),
+            nn.Linear(512, self.hyperparams["n_out"]),
         )
 
     def forward(self, x):
