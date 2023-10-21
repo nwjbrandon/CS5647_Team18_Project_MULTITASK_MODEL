8c290fc7060ee9d8e2212b4f19d405b811a4961d
nwjbrandon <nwjbrandon@gmail.com>
2023-10-21 12:31:36 +0800

    feat: code to extractor feature extractor

--- pretrained_model.py
+++ pretrained_model.py
@@ -1,79 +1,33 @@
 import torch
-import torch.nn as nn
 
-
-class Model(nn.Module):
-    def __init__(self, hyperparams):
-        super().__init__()
-        self.hyperparams = hyperparams
-        self.feature_extractor = nn.Sequential(
-            nn.Conv2d(1, 32, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(32),
-            nn.MaxPool2d(2),
-            nn.Conv2d(32, 64, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(64),
-            nn.MaxPool2d(2),
-        )
-
-        self.prediction = nn.Sequential(
-            nn.AdaptiveAvgPool2d(1),
-            nn.Flatten(start_dim=1),
-            nn.Linear(64, self.hyperparams["n_tones"]),
-        )
-
-    def forward(self, x):
-        x = self.feature_extractor(x)
-        out = self.prediction(x)
-        return x, out
-
-
-class PreTrainedModel(nn.Module):
-    def __init__(self, hyperparams):
-        super().__init__()
-        self.hyperparams = hyperparams
-        self.feature_extractor = nn.Sequential(
-            nn.Conv2d(1, 32, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(32),
-            nn.MaxPool2d(2),
-            nn.Conv2d(32, 64, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(64),
-            nn.MaxPool2d(2),
-        )
-
-    def forward(self, x):
-        out = self.feature_extractor(x)
-        return out
-
+from models import MultiTaskClassificationModel
 
 if __name__ == "__main__":
     hyperparams = {
-        "batch_size": 64,
-        "n_tones": 4,
-        "learning_rate": 0.01,
-        "test_size": 0.3,
-        "random_state": 1,
-        "n_epochs": 10,
+        "dataset": "MULTITASK",
+        "batch_size": 32,
+        "n_classes": 2,
+        "learning_rate": 0.003,
+        "n_epochs": 30,
+        "device": "mps",
+        "test_size": 0.2,
+        "random_state": 42,
         "n_mfcc": 128,
         "max_pad": 60,
+        "n_tones": 4,
+        "n_pinyins": 410,
     }
+
+    model = MultiTaskClassificationModel(hyperparams)
+    weights = torch.load("MULTITASK_model_25.pth")["model_state_dict"]
+    model.load_state_dict(weights)
+    model.eval()
     with torch.no_grad():
-        model = Model(hyperparams)
-        model.eval()
-        weights = torch.load("ckpts/pytorch_model_9.pth")["model_state_dict"]
-        model.load_state_dict(weights)
-
-        preTrainedModel = PreTrainedModel(hyperparams)
-        preTrainedModel.feature_extractor = model.feature_extractor
-
+        # Random Input
         inp = torch.rand(2, 1, 128, 60)
-        embeddings1, out = model(inp)
-        embeddings2 = preTrainedModel(inp)
-        print("Embeddings 1:")
-        print(embeddings1[0, 0, 0, :5])
-        print("Embeddings 2:")
-        print(embeddings2[0, 0, 0, :5])
-        assert torch.allclose(embeddings1, embeddings2)
+        tone_out, pinyin_out = model(inp)
+        print(tone_out.shape, pinyin_out.shape)
+
+        # Save only the feature extractor
+        feature_extractor = model.feature_extractor
+        torch.save(feature_extractor, "feature_extractor.pth")
